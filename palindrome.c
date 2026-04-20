b1c86362a68884ef70e7c915d64e6ca7da60ec0e
MurtazaHaider005 <smhaider005@gmail.com>
2026-04-21 03:56:06 +0530

    Implement palindrome check in palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int main() {
+    int n=121, rev=0, rem, temp=n;
+    while(n>0){
+        rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+    }
+    if(temp==rev)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+}
